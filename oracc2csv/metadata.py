6bfdd420ec67e48be24883e67bceda3129e44084
Tom Elliott <ipse@paregorios.org>
2022-06-24 15:22:54 -0500

    stub metadata module

---
+++ oracc2csv/metadata.py
@@ -0,0 +1,12 @@
+# This file is part of "oracc2csv"
+# by Tom Elliott
+# (c) Copyright 2022 by Tom Elliott
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+"""
+Python 3 package template (changeme)
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
